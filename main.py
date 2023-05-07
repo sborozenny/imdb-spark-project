95d20d9c6a871a2814204fb1f0cc9fb9d1044d6f
Serg Borozennyi <sborozenny@gmail.com>
2023-05-07 10:41:18 +0300

    Create skeleton 2

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pyspark.sql.types as t
+import pyspark.sql.functions as f
+
+
 def main():
     from pyspark import SparkConf, SparkContext
     from pyspark.sql import SparkSession
@@ -6,8 +10,13 @@ def main():
     sc = SparkContext.getOrCreate(conf=conf)
     spark = SparkSession(sc)
 
-    movies_df = spark.read.csv("imdb-data/name.basics.tsv.gz")
-    movies_df.show()
+    movies_df = spark.read.options(sep=r'\t').csv("imdb-data/name.basics.tsv.gz", header=True)
+    titles_df = spark.read.options(sep=r'\t').csv("imdb-data/title.akas.tsv.gz", header=True)
+    res = titles_df.filter(f.col('region') == 'UA').select('title', 'region')
+    res.write.csv('result/res1', header=True, mode='overwrite')
+    #movies_df.show()
+    res.show()
+
 
 if __name__ == "__main__":
     main()
